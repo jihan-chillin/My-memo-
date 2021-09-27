e921bcb08afb6468567e270c6e1dad98fa229e0e
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-09-27 14:46:12 +0900

    next.js + React 설정

--- front/Components/Applayout.js
+++ front/Components/Applayout.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import Link from 'next/link'; // next.js의 라우터
 
 const Applayout = ({children}) =>{
     return(
         <div>
             <div>공통메뉴</div>
+            <link href = "/"><a>노드버드</a></link>
+            <link href = "/profile"><a>프로필</a></link>
+            <link href = "/signup"><a>회원가입</a></link>
             {children}
         </div>
     )
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Applayout from '../Components/Applayout';
+import Link from 'next/link'; // next.js의 라우터
 
 const Signup = () =>{
-    return <Applayout>회원가입 페이지</Applayout>
+    return (
+        <Applayout>회원가입</Applayout>
+    )
 };
 
 export default Signup;
