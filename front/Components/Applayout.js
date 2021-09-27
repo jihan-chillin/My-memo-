58a3cd042a8b071e9ac4d8dc8eab3a6e504a2569
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-09-27 15:10:30 +0900

    Link + eslint 매핑

--- front/Components/Applayout.js
+++ front/Components/Applayout.js
@@ -1,16 +1,16 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import Link from 'next/link'; // next.js의 라우터
+import Link from "next/dist/client/link";
 
 const Applayout = ({children}) =>{
     return(
         <div>
-            <div>공통메뉴</div>
-            <link href = "/"><a>노드버드</a></link>
-            <link href = "/profile"><a>프로필</a></link>
-            <link href = "/signup"><a>회원가입</a></link>
+            <div>
+                <Link href = "/"><a>노드버드</a></Link>
+                <Link href = "/profile"><a>프로필</a></Link>
+                <Link href = "/signup"><a>회원가입</a></Link>
+            </div>
             {children}
-            
         </div>
     )
 };
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Applayout from '../Components/Applayout';
-import Link from 'next/link'; // next.js의 라우터
 
 
 const Signup = () =>{
