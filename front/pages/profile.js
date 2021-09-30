b8988ed093d2f98a0495b79aeb600a3472dd6030
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-09-30 09:11:52 +0900

    login 시 유저 프로필 화면 띄우기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Applayout from '../Components/Applayout';
+import NicknameEditForm from '../Components/NicknameEditform';
+import FollowList from '../Components/FollowList';
 import Head from 'next/head';
 
 const Profile = () => {
+
+    // dummy data
+    const follwingList = [{nickname : '코줍'}, {nickname : '즤하늬'}, {nickname : '프로독학러'}];
+    const followerList = [{nickname : '코줍'}, {nickname : '즤하늬'}, {nickname : '프로독학러'}];
+
     return (
         // children : Applayout 아래에 있는 요소
         <>
         <Head>
             <title>내 프로필 | NodeBird</title>
         </Head>
-        <Applayout>내 프로필</Applayout>
+        <Applayout>
+            <NicknameEditForm/>
+            <FollowList header="팔로잉 목록" data={follwingList}/>
+            <FollowList header="팔로워 목록" data={followerList}/>
+        </Applayout>
         </>
 
     )
